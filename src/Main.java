35152a05f8a1931c64584f2cda960823afea87f5
Light <lightme42@yandex.ru>
2020-05-01 02:32:54 +0800

    pushing a project to github

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static boolean isMatched = false;
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Write any number from 0 to 1000000000");
+        int yourNumber = 0;
+        try {
+            yourNumber = Integer.parseInt(reader.readLine());
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        //int randomNumber =(int) (Math.random() * 1_000_000_000);
+        //System.out.println("I guess number: " + randomNumber);
+        Thread timer = new Thread(() -> {
+            int i = 0;
+            try {
+                while (!isMatched) {
+                    System.out.println("timer: " + i + " seconds");
+                    i++;
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        timer.start();
+        int finalYourNumber = yourNumber;
+        Thread guess = new Thread(new Runnable() {
+            int guessNumber;
+            @Override
+            public void run() {
+                while (!isMatched) { 
+                   guessNumber = (int) (Math.random() * 1_000_000_000);
+                   if (guessNumber == finalYourNumber) {
+                       isMatched = true;
+                       System.out.println("I'm Done! Your number is: " + guessNumber);
+                   }
+                }
+            }
+        });
+        guess.start();
+    }
+}
